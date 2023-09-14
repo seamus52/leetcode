72b075bf8f929069afd86db65939a3c2a1976a9e
seamus52 <7493527+seamus52@users.noreply.github.com>
2023-09-14 14:17:32 -0700

    9/14/23 changes

--- largest_rectangle_in_histogram_optimal.py
+++ largest_rectangle_in_histogram_optimal.py
@@ -1,16 +1,31 @@
+# time: O(n^3)
+# space: O(1)
+# solution uses monotonic stack
+# x coordinate represented by heights array index
+# y coordinate represented by heights[x]
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        # init x coordinate
         stack = [-1]
-        max_area = 0
-        for i in range(len(heights)):
-            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
-                current_height = heights[stack.pop()]
-                current_width = i - stack[-1] - 1
-                max_area = max(max_area, current_height * current_width)
-            stack.append(i)
-
-        while stack[-1] != -1:
-            current_height = heights[stack.pop()]
-            current_width = len(heights) - stack[-1] - 1
-            max_area = max(max_area, current_height * current_width)
-        return max_area
+        
+        # add zero as dummy tail 
+        heights.append(0)
+        
+        # area of rectangle
+        max_rect = 0
+        
+        # scan each x coordinate and y coordinate
+        for x, y in enumerate(heights):
+            # while current height is lower than previous
+            while heights[stack[-1]] > y:
+            # update rectangle area from previous heights
+                # get height
+                h = heights[stack.pop()]
+                # compute width
+                w = x - stack[-1] - 1 
+                # update maximal area
+                max_rect = max(max_rect, h * w)
+            # push current x coordinat into stack
+            stack.append(x)
+            
+        return max_rect
